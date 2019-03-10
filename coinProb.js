6d33acb10f725bfc0a34d6dbf1cecd5dc8688885
Sriram-rengasamy <Sriram.renga@gmail.com>
2019-03-09 21:37:49 -0800

    chnags

--- coinProb.js
+++ coinProb.js
@@ -1,3 +1,17 @@
-function findMinimumCoinsForSum(){
-    
+findMinimumCoinsForSum([1,3,5],11);
+
+function findMinimumCoinsForSum(coins, sum){
+   let minSumArr = new Array(sum + 1).fill(Number.MAX_SAFE_INTEGER);
+   minSumArr[0] = 0;
+
+   for(let currSum = 1; currSum <= sum; currSum++){ /* sum - 1,2,3,....11 - find for previous sum */
+     for(let currCoinValue = 0; currCoinValue < coins.length; currCoinValue++){ /* each coin value */
+         if(coins[currCoinValue] <= currSum &&  /* coin value should be less than or equal to sum to use it */
+            minSumArr[currSum - coins[currCoinValue]] + 1 < minSumArr[currSum]){
+              minSumArr[currSum] = minSumArr[currSum - coins[currCoinValue]] + 1;
+         }
+     }
+   }
+   console.log(minSumArr);
+
 }
---
+++ longestIncreasingSequence.js
@@ -0,0 +1,5 @@
+
+function findLongIncrSeq(arr){
+let lis = new Array(arr.length).fill(1);
+
+}
